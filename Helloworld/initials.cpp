6a6e91c9aaeb7bcaa4f237617bcaa3992573e992
Sri Preetham Sai Ravula <preethamsai.ravula@gmail.com>
2021-12-23 16:10:59 -0800

    Added C++ helloworld chapter and initials project

---
+++ Helloworld/initials.cpp
@@ -0,0 +1,13 @@
+//FunFact: I like cooking Indian food
+# include <iostream>
+
+int main()
+{
+  std::cout << "RRRR    sss   pppp    sss \n";
+  std::cout << "R   R  s   s  p   p  s   s\n";
+  std::cout << "R   R  s      p   p  s    \n";
+  std::cout << "RRRR    sss   pppp    sss \n";
+  std::cout << "R R        s  p          s\n";
+  std::cout << "R  R   s   s  p      s   s\n";
+  std::cout << "R   R   sss   p       sss \n";
+}
